19cb756c889377dd213f9a7c1683ee2e0d6229a2
Roman Markelov <r.markelov@n1.ru>
2021-07-21 08:19:40 +0300

    Remove unsed plugin

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,10 +20,7 @@ export default {
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-    // ssr: false to only include it on client-side
-    { src: '~/plugins/entry-client.js', ssr: false }
-  ],
+  plugins: [],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
   components: true,
